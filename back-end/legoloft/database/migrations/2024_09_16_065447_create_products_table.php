763db60f00f3b2dc1f97f33afe7e4f812090e05a
littlegirl17 <khakha5087@gmail.com>
2024-09-16 18:59:06 +0700

    mig

---
+++ back-end/legoloft/database/migrations/2024_09_16_065447_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->text('image');
+            $table->integer('quantity')->default(1); // số lượng tồn kho
+            $table->unsignedBigInteger('category_id'); // Định nghĩa cột category_id
+            $table->foreign('category_id') // Tạo ràng buộc khóa ngoại
+                ->references('id') // Cột id trong bảng categories
+                ->on('categories') // Tên bảng categories
+                ->onUpdate('restrict') // Hành động khi cập nhật
+                ->onDelete('restrict'); // Hành động khi xóa
+                $table->decimal('price', 15, 2); // Cột price không có auto_increment
+                $table->decimal('discount_price', 15, 2);
+            $table->integer('view');
+            $table->tinyInteger('outstanding')->default(0);
+            $table->tinyInteger('status')->default(0);
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
