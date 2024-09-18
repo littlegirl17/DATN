917c97e915fefcd27497208f09f4a2b8519676d1
littlegirl17 <khakha5087@gmail.com>
2024-09-18 15:04:21 +0700

    k

--- back-end/legoloft/resources/views/login.blade.php
+++ back-end/legoloft/resources/views/login.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.layout')
 @section('title', 'Đăng nhập')
 @section('content')
+    <!-- START MAIN -->
     <div class="bg_login">
         <div class="container">
             <div class="login_main">
@@ -15,12 +16,25 @@
                     <div class="login_item">
                         <label for="">Mật khẩu</label>
                         <input type="password" placeholder="Nhập mật khẩu" />
+                        <label for="" class="login_forgetpw"><a href="" class="">Quên mật
+                                khẩu</a></label>
                     </div>
                     <div>
                         <button class="btn_login">Đăng nhập</button>
+                    </div>
+                    <div class="border-container-login">
+                        <hr class="border-left" />
+                        <span>Hoặc</span>
+                        <hr class="border-right" />
+                    </div>
+                    <div class="login_regis">
+                        <span>Bạn chưa có tài khoản?</span>
+                        <a href="">
+                            <span>Đăng ký thành viên<i class="fa-solid fa-arrow-right ps-1"></i></span></a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- END MAIN -->
 @endsection
